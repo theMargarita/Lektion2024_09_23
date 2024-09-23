8e985f2705519ebc441cc8259fce3c9552c56522
alexanderek <Alexander.ek@live.se>
2024-09-23 13:53:15 +0200

    Skapat meny

--- Program.cs
+++ Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
+            bool isRunning = true;
+            Console.WriteLine("Välkommen till miniräknaren!");
+            while (true)
+            {
+                Console.WriteLine("Var god välj vad du vill göra");
+                Console.WriteLine("1. Addera");
+                Console.WriteLine("2. Subtrahera");
+                Console.WriteLine("3. Multiplicera");
+                Console.WriteLine("4. Dividera");
+                Console.WriteLine("5. Avsluta miniräknaren");
+                string input = Console.ReadLine();
 
+                switch (input)
+                {
+                    case "1":
+                        //metod för att addera
+                        break;
+                    case "2":
+                        //metod för att subtrahera
+                        break;
+                    case "3":
+                        //metod för att multiplicera
+                        break;
+                    case "4":
+                        //metod för att dividera
+                        break;
+                    case "5":
+                        isRunning = false;
+                    default:
+                        Console.WriteLine("Felaktig inmatning, försök igen");
+                        break;
+                }
+
+            }
         }
     }
 }
